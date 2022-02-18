fab00d2a9c4fddff9dadea76b40c7b93eaa55056
Adam Uhlíř <uhlira@seznam.cz>
2022-02-18 17:13:21 +0100

    pridany projekty

---
+++ C#/TCPServerExample/TCPServerExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServerExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener TcpListener = new TcpListener(IPAddress.Parse("192.168.112.123"), 5000);
+            TcpListener.Start();
+
+            Console.WriteLine("TCP server started listen");
+            TcpClient t = TcpListener.AcceptTcpClient();
+            ClientHandle(t);
+
+            Console.ReadKey();
+        }
+
+        private static void ClientHandle(object obj)
+        {
+            TcpClient t = obj as TcpClient;
+
+            Console.WriteLine("TCP client connected from " + ((IPEndPoint)t.Client.RemoteEndPoint).Address + ":" + ((IPEndPoint)t.Client.RemoteEndPoint).Port);
+            
+            t.Close();
+        }
+    }
+}
